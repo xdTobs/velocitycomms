1ef1e05e5020f33132c0954af0d9201feaeddbb4
tobia <tobias@schonau>
2026-06-14 23:21:04 +0200

    init

---
+++ src/client/java/org/tobs/velocitycomms/client/VelocitycommsDataGenerator.java
@@ -0,0 +1,12 @@
+package org.tobs.velocitycomms.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VelocitycommsDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
---
+++ src/main/java/org/tobs/velocitycomms/Velocitycomms.java
@@ -0,0 +1,10 @@
+package org.tobs.velocitycomms;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Velocitycomms implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
